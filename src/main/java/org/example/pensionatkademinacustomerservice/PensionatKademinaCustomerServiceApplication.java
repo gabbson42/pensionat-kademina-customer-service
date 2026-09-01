5d66cd6ccbf867fb8c340d9253d576bd7cd3d4c1
Gabriel Landvik Åkerman <gabriel.landvikakerman@gmail.com>
2026-09-01 15:10:24 +0200

    Initial commit

---
+++ src/main/java/org/example/pensionatkademinacustomerservice/PensionatKademinaCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.pensionatkademinacustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionatKademinaCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensionatKademinaCustomerServiceApplication.class, args);
+    }
+
+}
