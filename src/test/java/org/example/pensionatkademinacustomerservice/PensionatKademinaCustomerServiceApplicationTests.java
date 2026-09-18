06c0b367a4cb61c210e393a6cf3c3daaf8db10e7
Gabriel Landvik Åkerman <gabriel.landvikakerman@gmail.com>
2026-09-18 17:31:59 +0200

    added ci file and adjusted main test class so contexLoads works.

--- src/test/java/org/example/pensionatkademinacustomerservice/PensionatKademinaCustomerServiceApplicationTests.java
+++ src/test/java/org/example/pensionatkademinacustomerservice/PensionatKademinaCustomerServiceApplicationTests.java
@@ -2,9 +2,20 @@ package org.example.pensionatkademinacustomerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
 
 @SpringBootTest
+@Testcontainers
 class PensionatKademinaCustomerServiceApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer db =
+            new MySQLContainer(
+                    "mysql:8");
 
     @Test
     void contextLoads() {
